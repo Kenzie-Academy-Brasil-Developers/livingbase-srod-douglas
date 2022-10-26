5ca3ebaca88ec921d6e6ea0211ff2d51d642f2b6
srod-douglas <srod.douglas@gmail.com>
2022-10-26 02:16:55 -0300

    feat: insert preferences to localStorage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,11 @@
 import { renderCardPost, renderCategories, renderSelectedCategory } from '../../scripts/render.js'
 import getPosts from '../../scripts/requests/posts.js'
 import { categories, getSelectedCategory } from '../../scripts/requests/categories.js'
+import { storage } from '../../scripts/localStorage.js'
+import { insertInLocalStorage } from '../../scripts/localStorage.js'
 
 const node = renderCategories()
-
+const redirects = document.querySelectorAll(".link")
 
 node.forEach((category)=>{
 
@@ -16,5 +18,18 @@ node.forEach((category)=>{
         }else{
             await getSelectedCategory(selected)
         }
+    }
+})
+
+redirects.forEach((choice)=>{
+
+    choice.onclick = (event) =>{
+        let category = event.target.classList[0].toLowerCase().replaceAll("ç", "c").replaceAll("ã", "a")
+        let pref = {
+            id: event.target.id,
+            category: `${category}`,
+        }
+        insertInLocalStorage(JSON.stringify(pref))
+
     }
 })
